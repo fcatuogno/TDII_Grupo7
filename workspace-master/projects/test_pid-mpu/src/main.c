1cd671c6708c8c6dfe35e5172a4362b2aba7331b
usuario-PC\usuario <daianav.casas@hotmail.com>
2018-12-11 14:49:45 -0300

    Se modifica la logica PID: estado inicial de una kte del PID y se realiza un programa para probar el funcionamiento del mismo

--- workspace-master/projects/test_pid-mpu/src/main.c
+++ workspace-master/projects/test_pid-mpu/src/main.c
@@ -23,14 +23,15 @@
 /* aceleracion+giroscopo */
 xQueueHandle 		ColaMPU;
 
-#define TIME_PERIOD_TASK	1/portTICK_RATE_MS // cada milisegundos
-#define deMS_aS 0.001
+#define TIME_PERIOD_TASK_inTICKS	1/portTICK_RATE_MS 	// cada milisegundos
+
+#define TIME_inS  1*portTICK_RATE_MS*0.001				// conversion de tick a segundos:
 
 /* Datos para el control PID */
 VALUES_K PID;
 VALUES_ERROR error[3];
 
-#define P 0
+#define P 1
 #define I 0
 #define D 0
 
@@ -65,7 +66,7 @@ static void initHardware(void)
 void LecturaDeDatos (void * param)
 {
 	portTickType xLastWakeTime;
-	const portTickType xPeriod=TIME_PERIOD_TASK;
+	const portTickType xPeriod=TIME_PERIOD_TASK_inTICKS;
 	static values_mpu datos;
 
 	// Inicializamos el xLastWakeTime con el tiempo actual al ingresar a la tarea
@@ -97,31 +98,40 @@ void ProcesamientoDeDatos (void * param)
 	static float x_out=0,y_out=0,z_out=0;
 
 	static float error_ant[3]={0,0,0};
-	static float iTime=0;
+	static float iTime=TIME_inS;
 
+	static bool FIRST_TIME=TRUE;
 	while(1){
 
 		/* Se espera que haya algun dato en la cola para procesar */
 		xQueueReceive( ColaMPU , & medicion , portMAX_DELAY );
 
 		/* Se toman los valores de los angulos */
-		MPU6050_GetAngle( & medicion ,& angulos ,  ((float)TIME_PERIOD_TASK * deMS_aS));
+		MPU6050_GetAngle( & medicion ,& angulos ,  ((float)TIME_inS));
 
 		/* Error proporcional */
-		error[0].p=angulos.yaw - x_ant;
-		error[1].p=angulos.roll - y_ant;
-		error[2].p=angulos.pitch - z_ant;
+		error[0].p=angulos.yaw; //- x_ant;
+		error[1].p=angulos.roll;// - y_ant;
+		error[2].p=angulos.pitch;// - z_ant;
 
 		/* Error integral */
-		error[0].i=error[0].i + error[0].p * iTime ;
-		error[1].i=error[1].i + error[1].p * iTime ;
-		error[2].i=error[2].i + error[2].p * iTime ;
+		error[0].i=( error[0].i + error[0].p ) * iTime ;
+		error[1].i=( error[1].i + error[1].p ) * iTime ;
+		error[2].i=( error[2].i + error[2].p ) * iTime ;
 
 		/* Error derivativo */
-		error[0].d= ( error[0].i - error_ant[0] )/ (iTime) ;
-		error[1].d= ( error[1].i - error_ant[1] )/ (iTime) ;
-		error[2].d= ( error[2].i - error_ant[2] )/ (iTime) ;
+		if(FIRST_TIME){
+		error[0].d= 0 ;
+		error[1].d= 0 ;
+		error[2].d= 0 ;
+		FIRST_TIME=FALSE;
+		}
+		else{
+		error[0].d= ( error[0].p - error_ant[0] )/ (iTime) ;
+		error[1].d= ( error[1].p- error_ant[1] )/ (iTime) ;
+		error[2].d= ( error[2].p - error_ant[2] )/ (iTime) ;
 
+		}
 		/* Se determinan las salidas por la ecuacion tipica del pid */
 		x_out= PID.Kp * error[0].p + PID.Ki * error[0].i + PID.Kd * error[0].d ;
 		y_out= PID.Kp * error[1].p + PID.Ki * error[1].i + PID.Kd * error[1].d ;
@@ -132,6 +142,9 @@ void ProcesamientoDeDatos (void * param)
 		y_ant=(float)Motor_Get(MOTOR_Y);
 		z_ant=(float)Motor_Get(MOTOR_Z);
 
+		Motor_Set(MOTOR_X,x_ant);
+		Motor_Set(MOTOR_Y,y_ant);
+		Motor_Set(MOTOR_Z,z_ant);
 		/* Se guarda el error del punto anterior */
 		error_ant[0]= error[0].p;
 		error_ant[1]= error[1].p;
@@ -145,28 +158,10 @@ int main(void)
 	// Creamos la cola que nos permite acceder a los datos del acelerometro
 	ColaMPU= xQueueCreate(5, sizeof(values_mpu));
 	// Tareas para manipular los datos y calcular los angulos:
-
 	xTaskCreate(LecturaDeDatos,(const char*) "ObtenerDatos", configMINIMAL_STACK_SIZE*2,0, tskIDLE_PRIORITY+1, NULL);
 	xTaskCreate(ProcesamientoDeDatos,(const char*) "DatosParaProcesar", configMINIMAL_STACK_SIZE*2,0, tskIDLE_PRIORITY, NULL);
-
-	PID_SetK(&PID, 1, KP);
-
-	while(1){
-	Motor_Set(1,0);
-	Motor_Set(1,30);
-	Motor_Set(1,45);
-	Motor_Set(1,60);
-	Motor_Set(1,45);
-	Motor_Set(1,90);
-    PWM1_ValueMatch(LPC_PWM1,CH4,ANGLE_P95);
-	 // Habilito los cambios en los MATCH's
-    PWM1_EnableMatchValue(LPC_PWM1,CH4);
-	/*	Reseteo todos los contadores */
-	PWM1_ResetCounters(LPC_PWM1);
-
-	}
 	// Iniciamos el Scheduler
-	//vTaskStartScheduler();
+	vTaskStartScheduler();
 
 	for(;;);
 	return 0;
